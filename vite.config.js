c3f06afe484bed2f702201566ba8ceb01e612eaa
rohitsnp <rohitsn@ikm.gov.in>
2025-05-28 23:34:27 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss()
+//   ]
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
+  base: '/portfolio/', // This must match your repo name
+  plugins: [react(),
     tailwindcss()
-  ]
+  ],
 })
